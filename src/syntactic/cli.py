b44b3ef0ce9048316f36cace2b006dbe079e1d7a
metatooling <metatooling@cordaz.com>
2019-12-31 00:35:50 +0000

    Add cli

--- src/syntactic/cli.py
+++ src/syntactic/cli.py
@@ -1,24 +1,21 @@
-"""
-Module that contains the command line app.
+"""CLI for custom syntax."""
 
-Why does this file exist, and why not put this in __main__?
-
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
-
-  - When you run `python -msyntactic` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``syntactic.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``syntactic.__main__`` in ``sys.modules``.
-
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
-"""
 import click
 
-from syntactic import app
+import syntactic.app
 
 
-@click.command()
+@click.group()
 def cli(**kwargs):
-    app.main(**kwargs)
+    """Commands for custom syntax."""
+
+
+@cli.command()
+@click.argument("file", type=click.File(mode="rb"))
+def show(file):
+    """Show the transformed code in a Python file."""
+    click.echo(syntactic.app.decode(file.read())[0])
+
+
+if __name__ == "__main__":
+    cli()
